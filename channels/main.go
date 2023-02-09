8cb00ef0ab7cfb951382e7075ad90b0ea6fc7365
griffinbird <benito.griffin@gmail.com>
2023-02-09 17:12:51 +1100

    refactor go routine for infinte loop on channel

--- channels/main.go
+++ channels/main.go
@@ -14,17 +14,25 @@ func main() {
 		"http://amazon.com",
 	}
 
+	c := make(chan string)
+
 	for _, link := range links {
-		go checkLink(link)
+		go checkLink(link, c)
+	}
+
+	for l := range c {
+		go checkLink(l, c)
 	}
 }
 
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Print(link, "might be done!")
+		c <- link
 		return // so we don't do anything else
 	}
 
 	fmt.Println(link, "is up!")
+	c <- link
 }
